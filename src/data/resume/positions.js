33ca93fb0f00f341cf3aff6cbb95067761a1755b
vishrut66 <108527286+vishrut66@users.noreply.github.com>
2023-06-04 17:27:25 +0530

    Update positions.js

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -3,7 +3,7 @@ const positions = [
     company: 'Ami Infootech',
     position: 'Blockchain developer',
     link: 'https://amiinfotech.com',
-    daterange: 'July 2022 - January 2023',
+    daterange: 'July 2022 - Present',
     points: [
       'Build numerous projects related to NFTs.',
       'Learnt build  positive  relationships with customers.',
